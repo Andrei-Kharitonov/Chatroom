fa52db7d4a32c935150be9cba6dd59f60d5ece5b
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2021-11-27 21:40:20 +0400

    initial commit

---
+++ client/src/components/Hello.tsx
@@ -0,0 +1,7 @@
+export default function Hello(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Hello from '../components/Hello';
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
