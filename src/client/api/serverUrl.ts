9638c183630e98bc76539f951b17d3db76b05f72
Andrei-Kharitonov <andr-haritonov@mail.ru>
2022-02-16 20:13:29 +0400

    hotfix

--- src/client/api/serverUrl.ts
+++ src/client/api/serverUrl.ts
@@ -1,3 +1,3 @@
 import { NODE_ENV, PORT } from "src/shared/env";
 
-export const serverUrl = NODE_ENV === 'production' ? `http://localhost:${PORT}` : 'https://chatroom-nest-next.herokuapp.com';
+export const serverUrl = NODE_ENV !== 'production' ? `http://localhost:${PORT}` : 'https://chatroom-nest-next.herokuapp.com';
